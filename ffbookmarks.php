dfbcde587de76008b46fc0171376d159b0744dc4
Offerel <14041522+Offerel@users.noreply.github.com>
2018-02-17 13:22:45 +0100

    initial version

---
+++ ffbookmarks.php
@@ -0,0 +1,54 @@
+<?php
+class ffbookmarks extends rcube_plugin
+{
+	public $task = 'mail';
+	
+	public function init() {
+		$rcmail = rcmail::get_instance();
+		
+		$this->add_texts('localization/', true);
+		$this->register_task('bookmarks');
+		$this->include_stylesheet($this->local_skin_path() . '/plugin.css');
+		$this->include_script('plugin.js');
+		
+		$this->add_button(array(
+			'label'	=> 'ffbookmarks.bookmarks',
+			'command'	=> 'bookmarks_cmd',
+			'id'		=> '7f3f3c06-5b85-4e7f-b527-d061478e9446',
+			'class'		=> 'button-bookmarks',
+			'classsel'	=> 'button-bookmarks button-bookmarks',
+			'innerclass'=> 'button-inner',
+			'type'		=> 'link'
+		), 'taskbar');
+		
+		$this->add_hook('render_page', array($this, 'add_bookmarks'));
+	}
+
+	function add_bookmarks($args) {
+		$rcmail = rcmail::get_instance();
+		$this->load_config();		
+		$bmfile = str_replace("%u", $rcmail->user->get_username(), $rcmail->config->get('bookmarks_path', false).$rcmail->config->get('bookmarks_filename', false));
+		$bookmarks = file_get_contents($bmfile);
+		$bookmarks = str_replace("<TITLE>Bookmarks</TITLE>", "<base target=\"_blank\">\n<TITLE>Bookmarks</TITLE>", $bookmarks);
+		$bookmarks = preg_replace("/<DD>[^>]*?</i", "<", $bookmarks);
+		$bookmarks = preg_replace("/<DT><H3 [^>]*? PERSONAL_TOOLBAR_FOLDER=\"true\">(.+?)<\/H3>/is", "</ol><H1>$1</H1>", $bookmarks);
+		$bookmarks = preg_replace("/<DT><H3 [^>]*? UNFILED_BOOKMARKS_FOLDER=\"true\">(.+?)<\/H3>/is", "<H1>$1</H1>", $bookmarks);
+		$bookmarks = preg_replace("/<H1>(.+?)<\/H1>/is", "<li>\n<label for=\"$1\">$1</label><input type=\"checkbox\" id=\"$1\">", $bookmarks);
+		$bookmarks = preg_replace("/<DT><H3\s(.+?)>(.+?)<\/H3>/is", "<li><label for=\"$2\">$2</label><input type=\"checkbox\" id=\"$2\">", $bookmarks);
+		$bookmarks = str_replace("<DT><A HREF=","<li class=\"file\"><A HREF=",$bookmarks);
+		$bookmarks = str_replace("</A>","</A></li>",$bookmarks);
+		$bookmarks = preg_replace("/<A (.+?)>(.+?)<\/A>/is", "<a title=\"$2\" $1>$2</a>", $bookmarks);
+		$bookmarks = str_replace("<DL><p>","<ol>",$bookmarks);
+		$bookmarks = str_replace("</DL><p>","</ol>",$bookmarks);
+		$bookmarks = str_replace("</DL>","</ol>",$bookmarks);		
+		$bookmarks.= "<span style=\"margin-left: 10px; font-size: 0.8em; color: rgba(190, 190, 190, 1.0); font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif; \">Date: " . date ("d.m.Y H:i:s.", filemtime($bmfile)) . "</span>";
+
+		$rcmail->output->add_footer("<div id=\"bookmarks\">".$bookmarks."</div>"); 
+		return $args;
+	}
+	
+	function bookmarks_cmd() {
+		$this->include_script('plugin.js');
+	}
+}
+?>
---
+++ plugin.js
@@ -0,0 +1,24 @@
+window.rcmail && rcmail.addEventListener('init', function(evt) {
+	rcmail.register_command('plugin.bookmarks_cmd', bookmarks_cmd, true);
+});
+
+//
+
+$( document ).ready(function() {
+	document.getElementById('7f3f3c06-5b85-4e7f-b527-d061478e9446').addEventListener("click", bookmarks_cmd);
+	
+	document.getElementById('bookmarks').addEventListener('click', function(e){
+		//console.log(e.target.tagName);
+		if(e.target.tagName=="A"){
+			console.log('link CLICKED');
+			bookmarks_cmd();
+		}
+	})
+});
+
+function bookmarks_cmd() {
+	if(document.getElementById('bookmarks').clientWidth != "300")
+		document.getElementById('bookmarks').style.width = "300px";
+	else
+		document.getElementById('bookmarks').style.width = "0";
+}
