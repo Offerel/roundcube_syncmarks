fa287d034730e005236449f9a7b8c866a6d842ae
Offerel <14041522+Offerel@users.noreply.github.com>
2018-02-17 14:11:07 +0100

    fixed small typo in classic skin

--- plugin.js
+++ plugin.js
@@ -8,7 +8,6 @@ $( document ).ready(function() {
 	document.getElementById('7f3f3c06-5b85-4e7f-b527-d061478e9446').addEventListener("click", bookmarks_cmd);
 	
 	document.getElementById('bookmarks').addEventListener('click', function(e){
-		//console.log(e.target.tagName);
 		if(e.target.tagName=="A"){
 			console.log('link CLICKED');
 			bookmarks_cmd();
