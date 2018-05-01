844420c79f30ebc65df9c170abe9d491b6164123
Offerel <14041522+Offerel@users.noreply.github.com>
2018-05-01 21:28:20 +0200

    bump version number to 1.0.3 to include JavaScript fix

--- plugin.js
+++ plugin.js
@@ -1,3 +1,11 @@
+/**
+ * Roundcube Firefox Bookmarks Plugin
+ *
+ * @version 1.0.3
+ * @author Offerel
+ * @copyright Copyright (c) 2018, Offerel
+ * @license GNU General Public License, version 3
+ */
 $(document).ready(function() {
 	$('#7f3f3c06-5b85-4e7f-b527-d061478e9446').on("click", bookmarks_cmd);
 	
