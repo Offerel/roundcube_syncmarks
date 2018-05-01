f09cec473ff29cc8b3ac417110db63ae4796c9f8
Offerel <14041522+Offerel@users.noreply.github.com>
2018-05-01 14:07:30 +0200

    small css fixes

--- plugin.js
+++ plugin.js
@@ -1,10 +1,5 @@
-/*
-window.rcmail && rcmail.addEventListener('init', function(evt) {
-	rcmail.register_command('plugin.bookmarks_cmd', bookmarks_cmd, true);
-});
-*/
 $(document).ready(function() {
-	document.getElementById('7f3f3c06-5b85-4e7f-b527-d061478e9446').addEventListener("click", bookmarks_cmd);
+	$('#7f3f3c06-5b85-4e7f-b527-d061478e9446').on("click", bookmarks_cmd);
 	
 	document.getElementById('bookmarks').addEventListener('click', function(e){
 		if(e.target.tagName=="A"){
