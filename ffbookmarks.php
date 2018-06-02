82b71903b24d2cf19fa9b7be4f1b7fc6bf5b8bd4
Offerel <14041522+Offerel@users.noreply.github.com>
2018-06-02 14:12:03 +0200

    you can now add bookmarks

--- ffbookmarks.php
+++ ffbookmarks.php
@@ -2,7 +2,7 @@
 /**
  * Roundcube Bookmarks Plugin
  *
- * @version 1.0.3
+ * @version 1.1.0
  * @author Offerel
  * @copyright Copyright (c) 2018, Offerel
  * @license GNU General Public License, version 3
@@ -15,8 +15,8 @@ class ffbookmarks extends rcube_plugin
 		$rcmail = rcmail::get_instance();
 		
 		$this->add_texts('localization/', true);
-		$this->register_task('bookmarks');
-		$this->include_stylesheet($this->local_skin_path() . '/plugin.css');
+		$this->register_task('ffbookmarks');
+		$this->include_stylesheet($this->local_skin_path().'/plugin.css');
 		$this->include_script('plugin.js');
 		
 		$this->add_button(array(
@@ -30,6 +30,44 @@ class ffbookmarks extends rcube_plugin
 		), 'taskbar');
 		
 		$this->add_hook('render_page', array($this, 'add_bookmarks'));
+		
+		$this->register_action('add_url', array($this, 'add_url'));
+	}
+	
+	function add_url() {
+		$rcmail = rcmail::get_instance();
+		$this->load_config();
+		$new_url = rcube_utils::get_input_value('_url', rcube_utils::INPUT_POST);
+		$bmfile = str_replace("%u", $rcmail->user->get_username(), $rcmail->config->get('bookmarks_path', false).$rcmail->config->get('bookmarks_filename', false));
+
+		if(file_exists($bmfile)) {
+			$bookmarks = file_get_contents($bmfile);
+			$end = strrpos($bookmarks, "</DL><p>");
+			$title_str = file_get_contents($new_url);
+			if(strlen($title_str)>0){
+				$title_str = trim(preg_replace('/\s+/', ' ', $title_str));
+				preg_match("/\<title\>(.*)\<\/title\>/i",$title_str,$title);
+				$date = time();
+				$meta_arr = get_meta_tags($new_url);
+				
+				if(strlen($meta_arr['keywords']) > 0){
+					$new_tags = "TAGS=\"".str_replace(" ", ",", $meta_arr['keywords'])."\"";
+				}
+				else
+					$new_tags = "";
+				
+				$new_bookmark = "\t<DT><A HREF=\"".$new_url."\" ADD_DATE=\"$date\" LAST_MODIFIED=\"$date\" LAST_CHARSET=\"UTF-8\" $new_tags>".trim($title[1])."</A>";
+				
+				if(strlen($meta_arr['description']) > 0){
+					$new_bookmark = utf8_encode($new_bookmark."\n\t\t<DD>".$meta_arr['description']."\n\t");
+				}
+			}
+			
+			$bookmarks = substr_replace($bookmarks, $new_bookmark, $end, 0);
+			file_put_contents($bmfile, $bookmarks);
+		}
+		
+		$rcmail->output->command('ffbookmarks/urladded', array('message' => 'URL is added.'));
 	}
 
 	function add_bookmarks($args) {
@@ -50,7 +88,7 @@ class ffbookmarks extends rcube_plugin
 			$bookmarks = str_replace("</DL><p>","</ol>",$bookmarks);
 			$bookmarks = str_replace("</DL>","</ol>",$bookmarks);		
 			$bookmarks.= "<span style=\"margin-left: 10px; font-size: 0.8em; color: rgba(190, 190, 190, 1.0); font-family: \"Helvetica Neue\", Helvetica, Arial, sans-serif; \">Date: " . date ("d.m.Y H:i:s.", filemtime($bmfile)) . "</span>";
-			$rcmail->output->add_footer("<div id=\"bookmarks\">".$bookmarks."</div>");
+			$rcmail->output->add_footer("<div id=\"bookmarkpane\"><div id=\"bookmarks\">".$bookmarks."</div><div id=\"add\" onclick=\"add_url();\">".$this->gettext('bookmarks_new').$name."</div></div>");
 			return $args;
 		}
 		else {
--- plugin.js
+++ plugin.js
@@ -1,15 +1,19 @@
 /**
  * Roundcube Firefox Bookmarks Plugin
  *
- * @version 1.0.3
+ * @version 1.1.0
  * @author Offerel
  * @copyright Copyright (c) 2018, Offerel
  * @license GNU General Public License, version 3
  */
+ window.rcmail && rcmail.addEventListener('init', function(evt) {
+	rcmail.register_command('plugin.ffbookmarks.add_url', add_url, true);
+});
+
 $(document).ready(function() {
 	$('#7f3f3c06-5b85-4e7f-b527-d061478e9446').on("click", bookmarks_cmd);
 	
-	document.getElementById('bookmarks').addEventListener('click', function(e){
+	document.getElementById('bookmarkpane').addEventListener('click', function(e){
 		if(e.target.tagName=="A"){
 			bookmarks_cmd();
 		}
@@ -17,11 +21,24 @@ $(document).ready(function() {
 });
 
 function bookmarks_cmd() {
-	if(document.getElementById('bookmarks').clientWidth != "300") {
-		document.getElementById('bookmarks').style.width = "300px";
-		document.getElementById('bookmarks').style.border = "thin solid #b2b8bf";
+	if(document.getElementById('bookmarkpane').clientWidth != "300") {
+		document.getElementById('bookmarkpane').style.width = "300px";
 	} else {
-		document.getElementById('bookmarks').style.width = "0";
-		document.getElementById('bookmarks').style.border = "0";
+		document.getElementById('bookmarkpane').style.width = "0";
 	}
+}
+
+function add_url() {
+	var url = prompt(rcmail.gettext('bookmarks_url', 'ffbookmarks'));
+    //if (url.length > 0 && url.startsWith("http")) {
+	if (url.length > 0) {
+		//rcmail.addEventListener('plugin.somecallback', some_callback_function);
+		rcmail.http_post('ffbookmarks/add_url', '_url=' + url);
+    }
+}
+
+function urladded(response)
+{
+	location.reload();
+	console.log(response.message);
 }
