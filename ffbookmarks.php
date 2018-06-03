1ccb629d06c807406d011b33435c2a35ed71c9c4
Offerel <14041522+Offerel@users.noreply.github.com>
2018-06-03 14:42:14 +0200

    added deleting a bookmark

--- ffbookmarks.php
+++ ffbookmarks.php
@@ -32,6 +32,36 @@ class ffbookmarks extends rcube_plugin
 		$this->add_hook('render_page', array($this, 'add_bookmarks'));
 		
 		$this->register_action('add_url', array($this, 'add_url'));
+		$this->register_action('del_url', array($this, 'del_url'));
+	}
+	
+	function del_url() {
+		$rcmail = rcmail::get_instance();
+		$this->load_config();
+		$new_url = rcube_utils::get_input_value('_url', rcube_utils::INPUT_POST);
+		$bmfile = str_replace("%u", $rcmail->user->get_username(), $rcmail->config->get('bookmarks_path', false).$rcmail->config->get('bookmarks_filename', false));
+		$del_url = rcube_utils::get_input_value('_url', rcube_utils::INPUT_POST);
+		
+		if(file_exists($bmfile)) {
+			$bookmarks = file_get_contents($bmfile);
+			$start = strrpos($bookmarks, $del_url) - 13;
+			$line_end = strpos($bookmarks,"\n",$start);
+			$new_line = strpos($bookmarks,"<",$line_end);
+			
+			switch(substr($bookmarks,$new_line,3)) {
+				case "<DD": $end = strpos($bookmarks,"\n",$new_line); break;
+				case "<DT": $end = strpos($bookmarks,"\n",$start); break;
+				default: $end = strpos($bookmarks,"\n",$start); break;
+			}
+			
+			$length = $end - $start;
+			$entry = substr($bookmarks,$start,$length);
+			$bookmarks = str_replace($entry,"",$bookmarks);
+			
+			file_put_contents($bmfile, $bookmarks);
+			
+			$rcmail->output->command('ffbookmarks/urladded', array('message' => "Bookmark deleted"));
+		}
 	}
 	
 	function add_url() {
@@ -43,10 +73,26 @@ class ffbookmarks extends rcube_plugin
 		if(file_exists($bmfile)) {
 			$bookmarks = file_get_contents($bmfile);
 			$end = strrpos($bookmarks, "</DL><p>");
-			$title_str = file_get_contents($new_url);
-			if(strlen($title_str)>0){
-				$title_str = trim(preg_replace('/\s+/', ' ', $title_str));
-				preg_match("/\<title\>(.*)\<\/title\>/i",$title_str,$title);
+			
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $new_url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_REFERER, $_SERVER['REQUEST_URI']);
+			curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+			curl_setopt($ch, CURLOPT_MAXREDIRS, 4);
+			curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
+
+			$data = curl_exec($ch);		
+			curl_close($ch);
+
+			$doc = new DOMDocument();
+			@$doc->loadHTML($data);
+			$nodes = $doc->getElementsByTagName('title');
+			$title = $nodes->item(0)->nodeValue;
+			
+			if(strlen($title) > 0) {
 				$date = time();
 				$meta_arr = get_meta_tags($new_url);
 				
@@ -56,18 +102,31 @@ class ffbookmarks extends rcube_plugin
 				else
 					$new_tags = "";
 				
-				$new_bookmark = "\t<DT><A HREF=\"".$new_url."\" ADD_DATE=\"$date\" LAST_MODIFIED=\"$date\" LAST_CHARSET=\"UTF-8\" $new_tags>".trim($title[1])."</A>";
+				$new_bookmark = "\t<DT><A HREF=\"".$new_url."\" ADD_DATE=\"$date\" LAST_MODIFIED=\"$date\" LAST_CHARSET=\"UTF-8\" $new_tags>".trim($title)."</A>";
 				
 				if(strlen($meta_arr['description']) > 0){
 					$new_bookmark = utf8_encode($new_bookmark."\n\t\t<DD>".$meta_arr['description']."\n\t");
 				}
+				
+				$bookmarks = substr_replace($bookmarks, $new_bookmark, $end, 0);
+				file_put_contents($bmfile, $bookmarks);
+				$rcmail->output->command('ffbookmarks/urladded', array('message' => 'URL is added.'));
 			}
-			
-			$bookmarks = substr_replace($bookmarks, $new_bookmark, $end, 0);
-			file_put_contents($bmfile, $bookmarks);
+			else {
+				$rcmail->output->command('ffbookmarks/urladded', array('message' => "Error. Cant add URL"));
+			}
 		}
 		
-		$rcmail->output->command('ffbookmarks/urladded', array('message' => 'URL is added.'));
+		
+	}
+	
+	public function get_content($URL){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $URL);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
 	}
 
 	function add_bookmarks($args) {
@@ -81,7 +140,7 @@ class ffbookmarks extends rcube_plugin
 			$bookmarks = preg_replace("/<DT><H3 [^>]*? UNFILED_BOOKMARKS_FOLDER=\"true\">(.+?)<\/H3>/is", "<H1>$1</H1>", $bookmarks);
 			$bookmarks = preg_replace("/<H1>(.+?)<\/H1>/is", "<li>\n<label for=\"$1\">$1</label><input type=\"checkbox\" id=\"$1\">", $bookmarks);
 			$bookmarks = preg_replace("/<DT><H3\s(.+?)>(.+?)<\/H3>/is", "<li><label for=\"$2\">$2</label><input type=\"checkbox\" id=\"$2\">", $bookmarks);
-			$bookmarks = str_replace("<DT><A HREF=","<li class=\"file\"><A target='_blank' HREF=",$bookmarks);
+			$bookmarks = str_replace("<DT><A HREF=","<li class=\"file\"><A onContextMenu=\"b_menu(event, this);\" target='_blank' HREF=",$bookmarks);
 			$bookmarks = str_replace("</A>","</A></li>",$bookmarks);
 			$bookmarks = preg_replace("/<A (.+?)>(.+?)<\/A>/is", "<a title=\"$2\" $1>$2</a>", $bookmarks);
 			$bookmarks = str_replace("<DL><p>","<ol>",$bookmarks);
--- plugin.js
+++ plugin.js
@@ -7,7 +7,7 @@
  * @license GNU General Public License, version 3
  */
  window.rcmail && rcmail.addEventListener('init', function(evt) {
-	rcmail.register_command('plugin.ffbookmarks.add_url', add_url, true);
+	//rcmail.register_command('plugin.ffbookmarks.add_url', add_url, true);
 });
 
 $(document).ready(function() {
@@ -20,6 +20,15 @@ $(document).ready(function() {
 	})
 });
 
+function b_menu(event, bookmark) {
+	event.preventDefault();
+	var question = rcmail.gettext('bookmarks_del', 'ffbookmarks').replace('%b%', bookmark.innerHTML);
+	if(confirm(question) == true) {
+		var url = encodeURIComponent(bookmark.href);
+		rcmail.http_post('ffbookmarks/del_url', '_url=' + url);
+	}
+} 
+
 function bookmarks_cmd() {
 	if(document.getElementById('bookmarkpane').clientWidth != "300") {
 		document.getElementById('bookmarkpane').style.width = "300px";
@@ -29,15 +38,14 @@ function bookmarks_cmd() {
 }
 
 function add_url() {
-	var url = prompt(rcmail.gettext('bookmarks_url', 'ffbookmarks'));
+	var url = encodeURIComponent(prompt(rcmail.gettext('bookmarks_url', 'ffbookmarks')));
     if (url.length > 0) {
 		if(url.startsWith("http") || url.startsWith("ftp"))
 			rcmail.http_post('ffbookmarks/add_url', '_url=' + url);
     }
 }
 
-function urladded(response)
-{
+function urladded(response) {
 	location.reload();
 	console.log(response.message);
 }
