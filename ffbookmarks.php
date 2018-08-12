2b55900e2152beda435aef97c074021f1f95ce7e
Offerel <14041522+Offerel@users.noreply.github.com>
2018-08-12 13:11:13 +0200

    Chrome Branch

--- ffbookmarks.php
+++ ffbookmarks.php
@@ -2,7 +2,7 @@
 /**
  * Roundcube Bookmarks Plugin
  *
- * @version 2.0.0
+ * @version 2.0.2
  * @author Offerel
  * @copyright Copyright (c) 2018, Offerel
  * @license GNU General Public License, version 3
